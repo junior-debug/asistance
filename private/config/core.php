016e24abf442efa5fd9d2c5c4d52ebcee4d156c3
jmartinez <jmartinez@undanet.com>
2024-05-28 13:34:22 +0200

    constantes bbdd

--- private/config/core.php
+++ private/config/core.php
@@ -3,9 +3,9 @@
 date_default_timezone_set('America/Caracas');
 
 #Constantes de conexión a BDD
-define('DB_HOST', 'localhost');
+define('DB_HOST', '192.168.10.50');
 define('DB_USER', 'root');            //desarrollos
-define('DB_PASS', '');            //prc333.c0m.v3
+define('DB_PASS', 'D3s4rr0ll0s++');            //prc333.c0m.v3
 define('DB_NAME', 'control_a_db');
 define('DB_PORT', '3306');
 
